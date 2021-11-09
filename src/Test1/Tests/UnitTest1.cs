6026b8dbaaa08b818ec2163f57003b8caf76c53b
yelena <yelenazvyagina@gmail.com>
2021-11-09 17:27:44 +0300

    test are not done

--- src/Test1/Tests/UnitTest1.cs
+++ src/Test1/Tests/UnitTest1.cs
@@ -1,21 +1,12 @@
-using System.Net;
 using NUnit.Framework;
-using Test1;
 
 namespace Tests
 {
     public class Tests
     {
-        private Server server;
-        private Client client;
-        private readonly string ip = "127.0.0.1";
-        private const int port = 8888;
-        
         [SetUp]
         public void Setup()
         {
-            server = new Server(port);
-            client = new Client(ip, port);
         }
 
         [Test]
