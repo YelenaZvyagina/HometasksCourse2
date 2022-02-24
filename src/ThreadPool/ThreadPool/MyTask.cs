530afc5f20eda6b1a14d1daa5e07748727dd9743
Yelena Zvyagina <yelenazvyagina@gmail.com>
2022-02-24 07:27:05 +0300

    ThreadPool init

---
+++ src/ThreadPool/ThreadPool/MyTask.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace ThreadPool
+{
+    public partial class MyThreadPool
+    { 
+        private class MyTask<TResult> : IMyTask<TResult> 
+        {
+            private readonly MyThreadPool _myThreadPool;
+            private readonly CancellationTokenSource _cancellationTokenSource;
+            private readonly Func<TResult> _taskFunction;
+            private AutoResetEvent are = new(false);
+            private ConcurrentQueue<Action> _continueTasks = new();
+            private object _lockObject = new();
+            public bool IsCompleted {get; set;}
+            public TResult Result {get; set;}
+            
+            public MyTask(Func<TResult> func, MyThreadPool myThreadPool)
+            {
+                _taskFunction = func;
+                _myThreadPool = myThreadPool;
+                _cancellationTokenSource = _myThreadPool._cancellationTokenSource;
+            }
+            
+            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> function)
+            {
+                lock (_cancellationTokenSource)
+                {
+                    if (_cancellationTokenSource.IsCancellationRequested)
+                    {
+                        throw new  InvalidOperationException("ThreadPool was shut down");
+                    }
+                }
+                var a = _myThreadPool.Submit(_taskFunction);
+                TNewResult F1() => function(a.Result);
+                if (IsCompleted)
+                {
+                    return _myThreadPool.Submit(F1);
+                }
+                var task = new MyTask<TNewResult>(F1, _myThreadPool);
+
+                lock (task)
+                {
+                    _continueTasks.Enqueue(task.RunTask);
+                }
+                return task;
+            }
+    
+            public void RunTask()
+            {
+                try
+                {
+                    Result = _taskFunction();
+                    IsCompleted = true;
+                }
+                catch (Exception e)
+                {
+                    throw new AggregateException($"Task failed with an exception {e}");
+                }
+            }
+        }
+    }
+}
---
+++ src/ThreadPool/ThreadPool/MyThreadPool.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace ThreadPool
+{
+    public partial class MyThreadPool
+    {
+        private int _threadCount;
+        private static readonly ConcurrentQueue<Action> TasksQueued = new();
+        private readonly CancellationTokenSource _cancellationTokenSource = new();
+        private readonly Thread[] _threads;
+        private readonly AutoResetEvent _newTask = new(false);
+
+        public MyThreadPool(int threadCount)
+        {
+            if (threadCount < 1) throw new ArgumentException("Amount of threads should be positive");
+            _threadCount = threadCount;
+            _threads = new Thread[threadCount];
+            
+            for (var i = 0; i < threadCount; i++)
+            {
+                _threads[i] = new Thread(() =>
+                {
+                    while (!_cancellationTokenSource.IsCancellationRequested)
+                    {
+                        if (TasksQueued.TryDequeue(out var task))
+                        {
+                            _newTask.Set();
+                            task();
+                        }
+                        else
+                        {
+                            _newTask.WaitOne();
+                        }
+                    }
+                });
+                _threads[i].Start();
+            }
+        }
+
+        public IMyTask<TResult> Submit<TResult>(Func<TResult> function)
+        {
+            lock (_cancellationTokenSource)
+            {
+                if (_cancellationTokenSource.IsCancellationRequested) 
+                    throw new OperationCanceledException("ThreadPool is already shut down, sorry(");
+                ArgumentNullException.ThrowIfNull(function);
+                var task = new MyTask<TResult>(function, this);
+                TasksQueued.Enqueue(task.RunTask);
+                return task;
+            }
+        }
+        
+        public void ShutDown()
+        {
+            lock (_cancellationTokenSource)
+            {
+                _cancellationTokenSource.Cancel();
+            }
+            foreach (var workingThread in _threads)
+            {
+                workingThread.Join();
+            }
+            _newTask.Reset();
+        }
+    }
+}
