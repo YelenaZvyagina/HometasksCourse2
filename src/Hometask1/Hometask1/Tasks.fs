1487704532fb0cbcb1948e893b4ef5370f2abe93
yelena <yelenazvyagina@gmail.com>
2022-02-28 12:14:41 +0300

    fixes

--- src/Hometask1/Hometask1/Tasks.fs
+++ src/Hometask1/Hometask1/Tasks.fs
@@ -1,45 +1,39 @@
 module Hometask1.Tasks
 
+open System
+
 let factorial number =
     if number < 0 then failwith "Attempt to calculate the factorial of negative number"
-    let rec _inner number acc =
-        if number = 0 then acc else _inner (number - 1) (acc * number)
-    _inner number 1
+    let rec factorialInner number acc =
+        if number = 0 then acc else factorialInner (number - 1) (acc * number)
+    factorialInner number 1
     
 let fibonacci n =
-    let rec _inner n acc1 acc2 =
+    let rec fibonacciInner n acc1 acc2 =
         if n = 0 then acc1
         elif n < 0 then failwith "It's unreal to find Fibonacci number if N is below zero"
-        else _inner (n - 1) acc2 (acc1 + acc2)
-    _inner n 0 1
+        else fibonacciInner (n - 1) acc2 (acc1 + acc2)
+    fibonacciInner n 0 1
     
 let reverseList list =
-    let rec _inner list newList =  
+    let rec reverseInner list newList =  
         match list with
-        | [] -> []
-        | [x] -> [x] @ newList
-        | h :: t -> _inner t ([h] @ newList)
-    _inner list []
+        | [] -> newList
+        | h :: t -> reverseInner t (h :: newList)
+    reverseInner list []
     
 let findInList number list =
-    let rec _inner number list acc =
+    let rec findInner number list acc =
         match list with
-        | [x] -> if x = number then acc else failwith "No number found sorry"
-        | h :: t -> if h = number then acc else _inner number t (acc+1)
-    _inner number list 0
-
-let rec twoPowN n =
-    if n = 1 then 2
-    else
-        match n%2 with
-        | 0 -> twoPowN (n/2) * twoPowN (n/2)
-        | 1 -> twoPowN (n/2) * twoPowN (n/2) * 2
+        | [] -> failwith "Number cannot be found in empty list"
+        | h :: t -> if h = number then acc else findInner number t (acc+1)
+    findInner number list 0
 
 // returns list [2^n ... 2^(n+m)]
 let listOfPowers n m =
-    let accStart = twoPowN n
-    let rec _inner m list acc =
+    let accStart = Math.Pow (2.0, float n)
+    let rec powersInner m list acc =
         match m with
-        | 0 -> list
-        | _ -> _inner (m-1) (list @ [acc*2]) (acc*2)
-    _inner (m-n) [accStart] accStart
+        | 0 -> reverseList list
+        | _ -> powersInner (m-1) ( acc*2.0 :: list ) (acc*2.0)
+    powersInner (m-n) [accStart] accStart
