fbcb3b8ee0271d3972e14bbfa8f78fb7f17786b1
Yelena Zvyagina <yelenazvyagina@gmail.com>
2022-03-31 21:10:51 +0300

    phone book init

---
+++ src/PhoneBook/PhoneBook/PhoneBook.fs
@@ -0,0 +1,34 @@
+﻿module PhoneBook.PhoneBook
+
+open System.IO
+
+[<Struct>]
+type PhoneBookRecord =
+    val name: string
+    val phoneNumber: int64
+    new (n, p) = {name = n; phoneNumber = p}
+    
+let addRecord name phone phoneBook = PhoneBookRecord(name, phone) :: phoneBook
+        
+let findPhoneByName name phoneBook =
+    phoneBook
+    |> List.filter (fun (x : PhoneBookRecord) -> (x.name = name))
+    |> List.map ( fun (x : PhoneBookRecord) -> x.phoneNumber)
+
+let findNameByPhone phone phoneBook =
+    phoneBook
+    |> List.filter (fun (x : PhoneBookRecord) -> (x.phoneNumber = phone))
+    |> List.map ( fun (x : PhoneBookRecord) -> x.name)
+    
+let printAllPhoneBook phoneBook = List.iter(fun (x : PhoneBookRecord) -> printfn $"Name %A{x.name}. Phone %A{string x.phoneNumber}") phoneBook
+
+let savePhoneBookToFile phoneBook filepath =
+    let dataToWriteInFile = List.map (fun (x : PhoneBookRecord) -> $"Name %A{x.name}. Phone %A{x.phoneNumber}") phoneBook
+    File.WriteAllLines(filepath, dataToWriteInFile)
+
+let getPhoneBookFromFile filepath =
+    let getDataFromString (s : string) =
+        let split = s.Split(' ')
+        if split.Length <> 2 then failwith "Incorrect data format in a file, \"name phoneNumber\" expected"
+        PhoneBookRecord(split.[0], int64 split.[1])
+    File.ReadAllLines filepath |> Seq.toList |> List.map getDataFromString
---
+++ src/PhoneBook/PhoneBookTests/Tests.fs
@@ -0,0 +1,37 @@
+module PhoneBookTests.Tests
+
+open Expecto
+open PhoneBook.PhoneBook
+
+
+[<Tests>]
+let phoneBookTests =
+    testList "Tests for phone book function"
+        [
+            testCase "Test for adding record function" <| fun _ ->
+                let testingBook = []
+                let addedRecord = addRecord "Nastya" 12345678789L testingBook
+                Expect.isTrue (addedRecord.Length = 1) ""
+                
+            testCase "test for find name function" <| fun _ ->
+                let testingBook = []
+                let addedRecord = addRecord "Nastya" 12345678789L testingBook
+                let foundName = findNameByPhone 12345678789L testingBook
+                Expect.sequenceEqual foundName ["Nastya"] ""
+                
+            testCase "test for find phone function" <| fun _ ->
+                let testingBook = []
+                let addedRecord = addRecord "Nastya" 12345678789L testingBook
+                let foundNumber = findPhoneByName "Nastya" testingBook
+                Expect.sequenceEqual foundNumber [12345678789L] ""
+                
+            testCase "tests for file functions" <| fun _ ->
+                let testingBook = []
+                let addedRecord = addRecord "Nastya" 12345678789L testingBook
+                let filepath = "C:\Users\ezvya\OneDrive\Desktop\study\HometasksSem3\src\PhoneBook\PhoneBookTests\pb.txt"
+                let savd = savePhoneBookToFile testingBook filepath
+                let read = getPhoneBookFromFile filepath
+                Expect.equal read.[1].name "Nastya" ""
+                Expect.sequenceEqual testingBook read ""
+       ]
+    
